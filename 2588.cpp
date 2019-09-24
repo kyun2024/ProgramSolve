debe4428c0a3be50476b1d4229e9af36ca07f1dc
kyun2024 <cune2024@gmail.com>
2019-09-24 10:32:06 +0900

    Add 2588

---
+++ 2588.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a{},b{};
+    cin >> a >> b;
+    cout << a*(b%10) << endl << a*((b/10)%10) << endl << a*((b/100)%10) << endl << a*b << endl;
+    return 0;
+}
