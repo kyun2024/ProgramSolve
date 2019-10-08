b7f92dd9ef9d86a108a96ee875c4afa752589586
kyun2024 <cune2024@gmail.com>
2019-10-09 00:15:09 +0900

    Add 15552

---
+++ 15552.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    int T{},a{},b{};
+    cin >> T;
+    for(int i=0;i<T;i++){
+        cin >> a >> b;
+        cout << a+b << "\n";
+    }
+    return 0;
+}
