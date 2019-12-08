af58fdbae986a4b25d1e4d2c8714ef23f530e115
kyun2024 <cune2024@gmail.com>
2019-12-08 13:59:30 +0900

    Add 2178

--- 2178.cpp
+++ 2178.cpp
@@ -33,7 +33,7 @@ int main(){
     qpath.push(path(0,0,0));
     while(!qpath.empty()){
         path cur = qpath.front();
-        cout << cur.x << " " << cur.y << " " << cur.c << endl;
+        //cout << cur.x << " " << cur.y << " " << cur.c << endl;
         if(cur.x == c-1 && cur.y == r-1)break;
         //Up
         if(cur.y > 0 && input[cur.y-1][cur.x]){
@@ -57,7 +57,7 @@ int main(){
         }
         qpath.pop();
     }
-    cout << qpath.front().c << endl;
+    cout << qpath.front().c+1 << endl;
 
     return 0;
 }
