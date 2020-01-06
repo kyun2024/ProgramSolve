21131182df30a79aaa29db263440ee7dff0b6870
kyun2024 <cune2024@gmail.com>
2020-01-06 22:52:26 +0900

    Add 2455

---
+++ 2455.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+auto main()->int{
+    using namespace std;
+    int i;
+    int a,b;
+    int sum=0;
+    int res=0;
+    for(i=0;i<4;i++){
+        cin >> a >> b;
+        sum += b-a;
+        if(res<sum)res=sum;
+    }
+    cout << res << endl;
+    return 0;
+}
