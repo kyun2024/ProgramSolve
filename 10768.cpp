18245e2c8e186ce6ae69d08aab952d425b2f287e
kyun2024 <cune2024@gmail.com>
2020-02-26 16:03:20 +0900

    Add 10768

---
+++ 10768.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int m,d;
+    cin >> m >> d;
+    if(m<2 || m==2 && d<18){
+        cout << "Before" << endl;
+    }
+    else if(m>2 || m==2 && d>18){
+        cout << "After" << endl;
+    }
+    else{
+        cout << "Special" << endl;
+    }
+    return 0;
+}
