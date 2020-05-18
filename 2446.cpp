4241b217ff02f14ed8967420364036796253214b
kyun2024 <cune2024@gmail.com>
2020-05-18 20:29:52 +0900

    Add 2446

---
+++ 2446.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    int i,j;
+    cin >> n;
+    for(i=n;i>=1;i--){
+        for(j=0;j<n-i;j++)cout << " ";
+        for(j=1;j<=2*i-1;j++)cout << "*";
+        cout << endl;
+    }
+    for(i=2;i<=n;i++){
+        for(j=0;j<n-i;j++)cout << " ";
+        for(j=1;j<=2*i-1;j++)cout << "*";
+        cout << endl;
+    }
+    return 0;
+}
