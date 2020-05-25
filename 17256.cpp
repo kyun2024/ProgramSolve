32bfa34b89c03765754e68942c1cd07b8f6e0f13
kyun2024 <cune2024@gmail.com>
2020-05-25 17:42:24 +0900

    Add 17256

---
+++ 17256.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a,b,c,d,e,f;
+    cin >> a >> b >> c >> d >> e >> f;
+    cout << d - c << " " << e/b << " " << f-a << endl;
+    return 0;
+}
