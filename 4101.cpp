63113832c7ced5a379a79c176f12bd178b52dab4
kyun2024 <cune2024@gmail.com>
2020-06-14 13:26:43 +0900

    Add 4101

---
+++ 4101.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a,b;
+    while(true){
+        cin >> a >> b;
+        if(a==b && b==0)break;
+        if(a>b)cout << "Yes\n";
+        else cout << "No\n";
+    }
+}
