9223cf3062ab8974c83772a292b629d9adf9ad7a
kyun2024 <cune2024@gmail.com>
2020-07-18 17:09:13 +0900

    Add 17356

---
+++ 17356.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int a,b;
+    double m,p;
+    cin >> a >> b;
+    cout << 1.0/(1.0+pow10l((b-a)/400.0)) << endl;
+}
