be1d23f7f30f0c5fd3296229247198d320b181e5
kyun2024 <cune2024@gmail.com>
2020-08-01 21:02:58 +0900

    Add 15657

---
+++ 15657.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int a[9];
+int sel[9];
+int n,m;
+
+void q(int i,int c){
+    sel[c]=i;
+    if(c==m){
+        for(int k=1;k<=m;k++){
+            cout << a[sel[k]] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+    for(int k=i;k<=n;k++){
+        q(k,c+1);
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin >> n >> m;
+    for(int i=1;i<=n;i++){
+        cin >> a[i];
+    }
+    sort(a+1,a+n+1);
+    q(1,0);
+    return 0;
+}
