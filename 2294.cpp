88ec40f46f838f48b7a98bbfe6f042eee1df62b5
kyun2024 <cune2024@gmail.com>
2020-11-01 12:29:33 +0900

    Add 2294

--- 2294.cpp
+++ 2294.cpp
@@ -1,52 +1,43 @@
 #include <iostream>
-#include <iomanip>
-#include <vector>
+#include <algorithm>
+#include <queue>
+
+int m[10001];
+int c[101];
 
 using namespace std;
 
+struct qe{
+    int n,c;
+};
+
 int main(){
-    int n,g;
-    int i;
-    vector<int> il,a1,a2;
-    cin >> n >> g;
-    il.resize(g+1,0);
-    a1.resize(g+1,0);
-    a2.resize(g+1,0);
-    for(i=0;i<n;i++){
-        cin >> il[i];
-    }
-    //Iter Lambda
-    auto calc_coin = [g](vector<int>& cur,vector<int>& bef,int coin){
-        int i,j;
-        for(i=0;i<min(coin,g+1);i++){
-            cur[i] = bef[i];
+    int n,k,i,rk{};
+    cin >> n >> k;
+    for(i=1;i<=n;i++)cin >> c[i];
+    sort(c+1,c+n+1,less<int>());
+    rk=1;
+    for(i=2;i<=n;i++)
+    {
+        if(c[i-1]!=c[i]){
+            rk++;
+            c[rk]=c[i];
         }
-        for(;i<=g;i++){
-            cur[i] = bef[i];
-            for(j=i;j>=0;j-=coin){
-                if(cur[i] == 0 && bef[j] != 0 || cur[i]>(i-j)/coin+bef[j] && bef[j] != 0){
-                    cur[i] = (i-j)/coin + bef[j];
-                }
+    }
+    qe f;
+    queue<qe> q;
+    q.push({k,0});
+    while(!q.empty() && m[0]==0){
+        f = q.front();
+        for(i=1;i<=rk;i++){
+            if(f.n-c[i]>=0 && m[f.n-c[i]]==0){
+                m[f.n-c[i]]=f.c+1;
+                q.push({f.n-c[i],f.c+1});
             }
         }
-    };
-
-    // at first, vector a1 is 'before' vector
-    // vector a2 is 'current' vector
-    // fill 1 at index 0
-    a1[0] = 1;
-
-    for(i=0;i<n;i++){
-        if(i%2==0){
-            calc_coin(a2,a1,il[i]);
-        }
-        else{
-            calc_coin(a1,a2,il[i]);
-        }
+        q.pop();
     }
-    if(n%2 == 0){
-        cout << a1[g]-1 << endl;
-    }
-    else cout << a2[g]-1 << endl;
+    m[0] = (m[0]==0?-1:m[0]);
+    cout << m[0] << endl;
     return 0;
 }
