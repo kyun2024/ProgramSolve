4031424e7c909637bb7634b6f09d56bf5ae4dfea
kyun2024 <cune2024@gmail.com>
2020-11-16 18:17:04 +0900

    Add 12833

---
+++ 12833.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cin >> a >> b >> c;
+    if(c%2==0){
+        cout << a << endl;
+    }
+    else cout << (a^b) << endl;
+    return 0;
+}
