ecce5d734868253f3839a5b0d71f6be636d3642b
kyun2024 <cune2024@gmail.com>
2020-12-20 19:03:54 +0900

    Add 15921

---
+++ 15921.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    if(n==0){
+        cout << "divide by zero" << endl;
+    }
+    else cout << "1.00" << endl;
+    return 0;
+}
