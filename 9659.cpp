295791b011220be767c07e32df91003619ebd25c
kyun2024 <cune2024@gmail.com>
2021-04-02 23:23:07 +0900

    Add 9659

---
+++ 9659.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    long long n;
+    cin >> n;
+    if(n%2==1){
+        cout << "SK" << endl;
+    }
+    else cout << "CY" << endl;
+    return 0;
+}
