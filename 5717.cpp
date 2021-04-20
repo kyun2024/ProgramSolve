597decfec81aa2700830dc49439d0534e28a8f18
kyun2024 <cune2024@gmail.com>
2021-04-20 23:13:59 +0900

    Add 5717

---
+++ 5717.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int m,f;
+    while(true){
+        cin >> m >> f;
+        if(m+f==0)break;
+        cout << m+f << endl;
+    }
+    return 0;
+}
