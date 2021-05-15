86bbc74b0567c52a6ac1b264eea92279ad79ba00
kyun2024 <cune2024@gmail.com>
2021-05-15 20:17:25 +0900

    Add 9656

---
+++ 9656.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    if(n%2){
+        cout << "CY" << endl;
+    }
+    else cout << "SK" << endl;
+    return 0;
+}
