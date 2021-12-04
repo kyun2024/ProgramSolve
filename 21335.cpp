2861053bd30d6d273cbac7caf31f9fe5714c2915
kyun2024 <cune2024@gmail.com>
2021-12-04 23:10:56 +0900

    Add 21335

---
+++ 21335.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    long long n;
+    cin >> n;
+    cout << fixed << setprecision(7) << 2.0*M_PI*sqrtl(n/M_PI) << endl;
+    return 0;
+}
