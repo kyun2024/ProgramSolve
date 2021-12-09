ed42ea273fcacdfb5099b95ce0305e0c9fca3fca
kyun2024 <cune2024@gmail.com>
2021-12-09 12:29:09 +0900

    Add 20353

---
+++ 20353.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    long long n;
+    cin >> n;
+    cout << fixed << setprecision(7) << sqrtl(n)*4.0 << endl;
+    return 0;
+}
