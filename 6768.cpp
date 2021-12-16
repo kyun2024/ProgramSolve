0c1aadfb94a5b8c322f8a6c0b5950b27bcc324d0
kyun2024 <cune2024@gmail.com>
2021-12-17 01:32:52 +0900

    Add 6768

---
+++ 6768.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    if(n<4)cout << 0 << '\n';
+    else cout << (n-1)*(n-2)*(n-3)/6 << '\n';
+    return 0;
+}
