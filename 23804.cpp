515763866a4cee98dbf80fb676c511595b8e6295
kyun2024 <cune2024@gmail.com>
2022-02-03 00:43:31 +0900

    Add 23804

---
+++ 23804.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    int i,j;
+    
+    cin >> n;
+    for(j=0;j<n;j++){for(i=0;i<n*5;i++)cout << "@"; cout << endl;}
+    for(j=0;j<n;j++){for(i=0;i<n;i++)cout << "@"; cout << endl;};
+    for(j=0;j<n;j++){for(i=0;i<n;i++)cout << "@"; cout << endl;};
+    for(j=0;j<n;j++){for(i=0;i<n;i++)cout << "@"; cout << endl;};
+    for(j=0;j<n;j++){for(i=0;i<n*5;i++)cout << "@"; cout << endl;}
+    return 0;
+}
