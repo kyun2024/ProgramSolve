73c2815dbd599437b1e83e156f9f483cb3853914
kyun2024 <cune2024@gmail.com>
2022-02-17 22:47:35 +0900

    Add 23805

---
+++ 23805.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+char map[][6]={
+    "@@@ @",
+    "@ @ @",
+    "@ @ @",
+    "@ @ @",
+    "@ @@@"
+};
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int i,j;
+    for(i=0;i<n*5;i++){
+        for(j=0;j<n*5;j++){
+            cout << map[i/n][j/n];
+        }
+        cout << endl;
+    }
+    return 0;
+}
