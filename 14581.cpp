4bac323b8405b6f6dafb6eb769b4c354f94080de
kyun2024 <cune2024@gmail.com>
2022-02-28 22:25:16 +0900

    Add 14581

---
+++ 14581.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string str;
+    cin >> str;
+    cout << ":fan::fan::fan:\n:fan::" << str << "::fan:\n:fan::fan::fan:\n";
+    return 0;
+}
