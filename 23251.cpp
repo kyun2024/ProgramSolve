583f586ace3a661a51a2f71f9f7035a5e71af791
kyun2024 <50358251+kyun2024@users.noreply.github.com>
2022-03-15 19:21:42 +0900

    Add 23251

---
+++ 23251.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cin.tie(0)->sync_with_stdio(false);
+
+    int T,n;
+    cin >> T;
+    while(T--){
+        cin >> n;
+        cout << n*23 << '\n';
+    }
+    return 0;
+}
