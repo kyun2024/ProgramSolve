48b736599f4cb75e66beceb65f72e4ca7e1a9b7a
kyun2024 <50358251+kyun2024@users.noreply.github.com>
2022-03-29 22:03:24 +0900

    Create 24724.cpp

---
+++ 24724.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int t,i;
+    cin >> t;
+    for(i=1;i<=t;i++){
+        cout << "Material Management " << i << endl;
+        cout << "Classification ---- End!" << endl;
+    }
+    return 0;
+}
