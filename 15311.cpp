077a876dffa2a05f6ac2cc5513a8ca92266668f0
kyun2024 <cune2024@gmail.com>
2024-08-21 15:35:02 +0900

    Add 15311

---
+++ 15311.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << 2000 << endl;
+    for(int i=0;i<1000;++i)cout << 1000 << " ";
+    for(int i=0;i<1000;++i)cout << 1 << " ";
+    cout << endl;
+}
